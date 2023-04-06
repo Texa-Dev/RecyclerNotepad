79bbc2f51d4dcdf215da16eeeaa497f8cd7a678e
Texa-Dev <artemtroc26@gmail.com>
2023-04-06 19:12:37 +0300

    test Main

--- app/src/main/java/com/example/recyclernotepad/MainActivity.java
+++ app/src/main/java/com/example/recyclernotepad/MainActivity.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.example.recyclernotepad.adapters.NoteAdapter;
+import com.example.recyclernotepad.adapters.NoteCursorAdapter;
 import com.example.recyclernotepad.data.DBManager;
 import com.example.recyclernotepad.data.Note;
 
@@ -63,6 +64,8 @@ public class MainActivity extends AppCompatActivity {
         }
 
         Cursor allCursor = manager.findAllCursor();
+        NoteCursorAdapter noteCursorAdapter = new NoteCursorAdapter(allCursor);
 
+        recyclerView.setAdapter(noteCursorAdapter);
     }
 }
